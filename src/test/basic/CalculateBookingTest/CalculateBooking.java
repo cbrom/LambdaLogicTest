0db1effe1bd95d0349ec010c269b5e0989dddcfe
cbrom abody <kb.cbrom@gmail.com>
2020-05-28 20:17:42 +0300

    basic test with one Booking in bookigs list added

--- src/test/basic/CalculateBookingTest/CalculateBooking.java
+++ src/test/basic/CalculateBookingTest/CalculateBooking.java
@@ -2,24 +2,36 @@ package test.basic.CalculateBookingTest;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.lambdalogic.test.booking.InvoiceRecipientBookingsCurrencyAmountsEvaluator;
 import com.lambdalogic.test.booking.exception.InconsistentCurrenciesException;
 import com.lambdalogic.test.booking.model.Booking;
+import com.lambdalogic.test.booking.model.CurrencyAmount;
+import com.lambdalogic.test.booking.model.Price;
+import com.lambdalogic.test.booking.util.TypeHelper;
 
 public class CalculateBooking {
 	InvoiceRecipientBookingsCurrencyAmountsEvaluator evaluator;
 	Long recipientPK;
+	InconsistentCurrenciesException inconsistentException;
+	
+	@Rule
+    public ExpectedException expectedException;
 
 	@Before
 	public void setUp() throws Exception {
 		evaluator = new InvoiceRecipientBookingsCurrencyAmountsEvaluator(); 
 		recipientPK = 1L;
+		
+		expectedException = ExpectedException.none();
 	}
 
 	@Test
@@ -28,9 +40,30 @@ public class CalculateBooking {
 		try {
 			evaluator.calculate(bookings, recipientPK);
 		} catch (InconsistentCurrenciesException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void basicTest() throws InconsistentCurrenciesException, ParseException {
+		List<Booking> bookings = new ArrayList<Booking>() {};
+		CurrencyAmount currencyAmount = new CurrencyAmount(TypeHelper.toBigDecimal("0.12"), "usd");
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		evaluator.calculate(bookings, recipientPK);
+		assertEquals(evaluator.getTotalAmount(), currencyAmount);
+	}
+	
+	@Test(expected = Exception.class) 
+	public void inconsistentCurrencyTest() throws InconsistentCurrenciesException, ParseException {
+		List<Booking> bookings = new ArrayList<Booking>() {};
+		inconsistentException = new InconsistentCurrenciesException(null, null);
+		expectedException.expect(InconsistentCurrenciesException.class);
+		
+		CurrencyAmount currencyAmount = new CurrencyAmount(TypeHelper.toBigDecimal("0.12"), "usd");
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "eur", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		evaluator.calculate(bookings, recipientPK);
+		assertEquals(evaluator.getTotalAmount(), currencyAmount);
 	}
 
 }
