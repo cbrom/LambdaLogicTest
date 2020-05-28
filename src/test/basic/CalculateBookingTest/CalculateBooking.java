dcff27261ad86af0c942607a2102dc9bd9aa9a7c
cbrom abody <kb.cbrom@gmail.com>
2020-05-28 20:52:25 +0300

    - rounding error test added - different recipientID test added

--- src/test/basic/CalculateBookingTest/CalculateBooking.java
+++ src/test/basic/CalculateBookingTest/CalculateBooking.java
@@ -46,16 +46,31 @@ public class CalculateBooking {
 	
 	@Test
 	public void basicTest() throws InconsistentCurrenciesException, ParseException {
-		List<Booking> bookings = new ArrayList<Booking>() {};
+		List<Booking> bookings = new ArrayList<Booking>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;};
 		CurrencyAmount currencyAmount = new CurrencyAmount(TypeHelper.toBigDecimal("0.12"), "usd");
 		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
 		evaluator.calculate(bookings, recipientPK);
+		
+		assertNotNull(evaluator.getTotalAmount());
+		assertNotNull(evaluator.getTotalOpenAmount());
+		assertNotNull(evaluator.getTotalPaidAmount());
+		
 		assertEquals(evaluator.getTotalAmount(), currencyAmount);
 	}
 	
 	@Test(expected = Exception.class) 
 	public void inconsistentCurrencyTest() throws InconsistentCurrenciesException, ParseException {
-		List<Booking> bookings = new ArrayList<Booking>() {};
+		List<Booking> bookings = new ArrayList<Booking>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;};
 		inconsistentException = new InconsistentCurrenciesException(null, null);
 		expectedException.expect(InconsistentCurrenciesException.class);
 		
@@ -63,6 +78,60 @@ public class CalculateBooking {
 		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
 		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "eur", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
 		evaluator.calculate(bookings, recipientPK);
+		assertEquals(evaluator.getTotalAmount(), currencyAmount);
+	}
+	
+	@Test
+	public void roundErrorsTest() throws InconsistentCurrenciesException, ParseException {
+		List<Booking> bookings = new ArrayList<Booking>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;};
+		CurrencyAmount totalAmountCurrency = new CurrencyAmount(TypeHelper.toBigDecimal("1.19"), "usd");
+		CurrencyAmount totalOpenCurrency = new CurrencyAmount(TypeHelper.toBigDecimal("0.29"), "usd");
+		CurrencyAmount totalPaidCurrency = new CurrencyAmount(TypeHelper.toBigDecimal("0.90"), "usd");
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		
+		evaluator.calculate(bookings, recipientPK);
+			
+		assertEquals(evaluator.getTotalAmount(), totalAmountCurrency);
+		assertEquals(evaluator.getTotalOpenAmount(), totalOpenCurrency);
+		assertEquals(evaluator.getTotalPaidAmount(), totalPaidCurrency);
+	}
+	
+	@Test
+	public void differentInvoiceRecipientTest() throws InconsistentCurrenciesException, ParseException {
+		List<Booking> bookings = new ArrayList<Booking>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;};
+		CurrencyAmount currencyAmount = new CurrencyAmount(TypeHelper.toBigDecimal("1.07"), "usd");
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 1L, null));
+		bookings.add(new Booking(1L, new Price(TypeHelper.toBigDecimal("0.10"), "usd", TypeHelper.toBigDecimal("19"), false), null, null, null, TypeHelper.toBigDecimal("0.09"), null, null, null, 2L, null));
+		
+		evaluator.calculate(bookings, recipientPK);
+			
 		assertEquals(evaluator.getTotalAmount(), currencyAmount);
 	}
 
